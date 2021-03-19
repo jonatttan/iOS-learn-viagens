a9f8c523af9390b621c3ab96b135029ee44927c7
Jonattan <jonattan.sousa@gmail.com>
2021-03-19 11:51:06 -0300

    Implementado tela de detalhes, conteudo alinhado sem uso de Scroll View.

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagemViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagemViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class DetalhesViagemViewController: UIViewController {
 
+    @IBOutlet weak var imagemPacoteViagem: UIImageView!
+    @IBOutlet weak var labelTituloPacoteViagem: UILabel!
+    @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
+    @IBOutlet weak var labelDataViagem: UILabel!
+    @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
+    
+    var pacoteSelecionado:PacoteViagem? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let pacote = pacoteSelecionado {
+            self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
+            self.labelTituloPacoteViagem.text = pacote.viagem.titulo
+            self.labelDescricaoPacoteViagem.text = pacote.descricao
+            self.labelDataViagem.text = pacote.dataViagem
+            self.labelPrecoPacoteViagem.text = pacote.viagem.preco
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -25,5 +41,8 @@ class DetalhesViagemViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
--- Alura Viagens/Pacotes/Controller/PacotesViagemViewController.swift
+++ Alura Viagens/Pacotes/Controller/PacotesViagemViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class PacotesViagemViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate {
+class PacotesViagemViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate, UICollectionViewDelegate {
 
     @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
     @IBOutlet weak var pesquisaViagens: UISearchBar!
     @IBOutlet weak var labelContadorPacotes: UILabel!
     
-    let listaAllViagem: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
-    var listaViagem: Array<Viagem> = []
+    let listaAllViagem: Array<PacoteViagem> = PacoteViagemDAO().retornaTodasAsViagens()
+    var listaViagem: Array<PacoteViagem> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +33,12 @@ class PacotesViagemViewController: UIViewController, UICollectionViewDataSource,
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
         //celulaPacote.backgroundColor = UIColor.blue
         
-        let viagemAtual = listaViagem[indexPath.item] //Item? sera?
+        let pacoteAtual = listaViagem[indexPath.item] //Item? sera?
 
-        celulaPacote.labelTitulo.text = viagemAtual.titulo
-        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
-        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
-        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
-//        celulaPacote.imagemViagem.layer.cornerRadius = 10
-//        celulaPacote.imagemViagem.contentMode = .scaleAspectFill
+        celulaPacote.labelTitulo.text = pacoteAtual.viagem.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(pacoteAtual.viagem.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(pacoteAtual.viagem.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: pacoteAtual.viagem.caminhoDaImagem)
         
         celulaPacote.layer.borderWidth = 0.5
         celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
@@ -48,11 +46,19 @@ class PacotesViagemViewController: UIViewController, UICollectionViewDataSource,
         return celulaPacote
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let pacote = listaViagem[indexPath.item]
+        let stoyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard?.instantiateViewController(withIdentifier: "detalhes") as! DetalhesViagemViewController
+        controller.pacoteSelecionado = pacote
+        self.present(controller, animated: true, completion: nil)
+    }
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         listaViagem = listaAllViagem
         if searchText != "" {
-            let filtroListaViagem = NSPredicate(format: "titulo contains %@", searchText)
-            let listaFiltrados:Array<Viagem> = (listaViagem as NSArray).filtered(using: filtroListaViagem) as! Array
+            let filtroListaViagem = NSPredicate(format: "titulo contains[cd] %@", searchText)
+            let listaFiltrados:Array<PacoteViagem> = (listaViagem as NSArray).filtered(using: filtroListaViagem) as! Array
             listaViagem = listaFiltrados
         }
         self.labelContadorPacotes.text = self.atualizaContadorLabel()
