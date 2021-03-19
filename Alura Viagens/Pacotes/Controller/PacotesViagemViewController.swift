815ee403af1fc8eae0f0a83d7ab1f9367bbb96bf
Jonattan <jonattan.sousa@gmail.com>
2021-03-19 00:10:31 -0300

    Adicionado Tab Bar Controller e filtragem de pacotes por titulo

--- Alura Viagens/Pacotes/Controller/PacotesViagemViewController.swift
+++ Alura Viagens/Pacotes/Controller/PacotesViagemViewController.swift
@@ -7,17 +7,22 @@
 
 import UIKit
 
-class PacotesViagemViewController: UIViewController, UICollectionViewDataSource {
+class PacotesViagemViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    @IBOutlet weak var pesquisaViagens: UISearchBar!
+    @IBOutlet weak var labelContadorPacotes: UILabel!
     
-    let listaViagem: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    let listaAllViagem: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    var listaViagem: Array<Viagem> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listaViagem = listaAllViagem
         colecaoPacotesViagem.dataSource = self
         colecaoPacotesViagem.delegate = self
-        //
+        pesquisaViagens.delegate = self
+        self.labelContadorPacotes.text = self.atualizaContadorLabel()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -41,6 +46,22 @@ class PacotesViagemViewController: UIViewController, UICollectionViewDataSource 
         celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
         celulaPacote.layer.cornerRadius = 8
         return celulaPacote
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        listaViagem = listaAllViagem
+        if searchText != "" {
+            let filtroListaViagem = NSPredicate(format: "titulo contains %@", searchText)
+            let listaFiltrados:Array<Viagem> = (listaViagem as NSArray).filtered(using: filtroListaViagem) as! Array
+            listaViagem = listaFiltrados
+        }
+        self.labelContadorPacotes.text = self.atualizaContadorLabel()
+        colecaoPacotesViagem.reloadData()
+        //print(searchText)
+    }
+    
+    func atualizaContadorLabel() -> String{
+        return listaViagem.count == 1 ? "1 pacote encontrado" : "\(listaViagem.count) pacotes encontrados"
     }
 }
 
